b3731ccfdccaa42824d9069b31305b34c2c45a18
Rodrigo <rodrigorael53@gmail.com>
2022-05-10 17:24:41 -0300

    finishing the settings for urql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
+import { Provider } from 'urql'
+import { client, ssrCache } from '../lib/urql'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // se eu já fiz uma query pelo lado ServerSide da minha aplicação e eu estiver
+  // utilizando os mesmos dados dessa query no clientSide ele não faz a query de
+  // novo
+  if (pageProps.urqlState) {
+    ssrCache.restoreData(pageProps.urqlState);
+  }
+  
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
