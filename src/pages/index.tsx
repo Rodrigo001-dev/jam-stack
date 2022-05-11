7dd6d39a304376407db5ab7fae0f4184b9c214d4
Rodrigo <rodrigorael53@gmail.com>
2022-05-11 15:18:33 -0300

    installing and configuring tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
-import { usePageQuery } from '../generated/graphql';
+import { GetServerSideProps } from 'next';
+
+import { usePageQuery, PageDocument } from '../generated/graphql';
+import { client, ssrCache } from '../lib/urql';
 
 export default function Home() {
+  // quando eu executo essa query aqui no client side a informação já está em chace
+  // por ela ter vindo do server side
   const [{ data }] = usePageQuery({ 
     variables: { 
       slug: 'home' 
@@ -11,3 +16,14 @@ export default function Home() {
     <h1>{data?.page.title}</h1>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // informação sendo carregada pelo server side
+  await client.query(PageDocument, { slug: 'home' }).toPromise();
+  
+  return {
+    props: {
+      urqlState: ssrCache.extractData()
+    }
+  }
+};
